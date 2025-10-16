12409c829bf83ae91990ea4e08c37f65ca3f540e
gabrielalimact <lgcl@ic.ufal.br>
2025-10-16 17:02:47 -0300

    feat: adiciona provedores de contexto e estilização ao layout principal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { ChallengeProvider } from '@/contexts/ChallengeContext';
+import { HabitsProvider } from '@/contexts/HabitsContext';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
@@ -13,12 +17,30 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ChallengeProvider>
+      <HabitsProvider>
+        <SafeAreaProvider>
+          <View style={styles.container}>
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+              <Stack
+                screenOptions={{
+                  contentStyle: { backgroundColor: '#edece4' },
+                }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+              </Stack>
+              <StatusBar style="auto" />
+            </ThemeProvider>
+          </View>
+        </SafeAreaProvider>
+      </HabitsProvider>
+    </ChallengeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#edece4',
+  },
+});
